9b1140c317eeab19977d86a604613e1c17e16de7
Sosohy <sh07202@naver.com>
2024-07-18 22:13:51 +0900

    [UPDATE] jwt 프로젝트 추가 및 세팅

--- jwt_practice/src/main/java/com/example/jwt_practice/controller/RestApiController.java
+++ jwt_practice/src/main/java/com/example/jwt_practice/controller/RestApiController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestApiController {
 
+    // 여기까지 세팅 완료
     @GetMapping("/home")
     public String home(){
         return "<h1>HOME</h1>";
