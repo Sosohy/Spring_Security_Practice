9480c7f07e4fa415863cb71e4869397918fcd7da
Sosohy <sh07202@naver.com>
2024-07-31 17:11:44 +0900

    [UPDATE] OAuth2UserService 구현

--- oauth2JWTPractice/src/main/java/com/example/oauth2JWTPractice/config/SecurityConfig.java
+++ oauth2JWTPractice/src/main/java/com/example/oauth2JWTPractice/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.example.oauth2JWTPractice.config;
 
+import com.example.oauth2JWTPractice.service.CustomOauth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,14 +13,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomOauth2UserService oauth2UserService;
+
+    public SecurityConfig(CustomOauth2UserService oauth2UserService) {
+        this.oauth2UserService = oauth2UserService;
+    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 
         http.csrf(AbstractHttpConfigurer::disable)               // csrf disable
                 .formLogin((formLogin) -> formLogin.disable())   // formLogin 방식 disable
-                .httpBasic((httpBasic) -> httpBasic.disable())  // HTTP Basic 인증 방식 disable
-                .oauth2Login(Customizer.withDefaults());
+                .httpBasic((httpBasic) -> httpBasic.disable()); // HTTP Basic 인증 방식 disable
+
+        http.oauth2Login((oauth2) -> oauth2
+                .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+                        .userService(oauth2UserService))));
 
         //경로별 인가 작업
         http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
